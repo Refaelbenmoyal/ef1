32371825f99fb61208d1806e14658ce8a418ca1e
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2021-01-10 21:56:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var entities = new CitiesDBEntities())
+            {
+                Console.WriteLine("\nPrint for Population > 30000: ");
+                entities.Districts.Where(p => p.Population > 30000).ToList().ForEach(r => Console.WriteLine(JsonConvert.SerializeObject(r)));
+                entities.SaveChanges();
+                object Cities = null;
+                var result = from c in Cities
+                             where c.Name
+                             select c;
+            }
+        }
+
+
+    }
+}
